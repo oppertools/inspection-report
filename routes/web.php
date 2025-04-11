03f7d4d2c490ec7ddb43a7cd1c8dae1daefdb214
Corentin Lecoeur <bonjour@clecoeur.fr>
2025-04-11 12:04:59 +0200

    fix: remove laravel horizon

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,7 @@
 use App\Domain\InspectionReports\Data\InspectionReportData;
 use App\Domain\InspectionReports\ViewModels\InspectionReportViewModel;
 use Illuminate\Support\Facades\Route;
-use Laravel\Horizon\Horizon;
 
 
 Route::get('inspection-report/{id}/show', [ \App\Http\Controllers\InspectionReport\InspectionReportController::class, 'show'])->name('inspection-report.show');
 Route::get('inspection-report/{id}', [ \App\Http\Controllers\InspectionReport\InspectionReportController::class, 'download'])->name('inspection-report.download');
-Route::get('/horizon', function () {
-	return redirect('/horizon'); // Horizon UI
-});
